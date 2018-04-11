749b3520528c3d18eff754ae3aa81570120a1668
Mateus1999Silva <38104612+Mateus1999Silva@users.noreply.github.com>
2018-04-11 16:18:31 -0300

    Update config.js

--- src/services/dbMongo/config.js
+++ src/services/dbMongo/config.js
@@ -1,7 +1,7 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.SALT_KEY = '**************************************';
 
 module.exports = {
-    connectionString: 'mongodb://admin:admin@ds245238.mlab.com:45238/api',
+    connectionString: 'mongodb://*******************************************',
     sendgridKey: 'SUA CHAVE',
     containerConnectionString: 'SUA CONNECTION STRING'
 };
